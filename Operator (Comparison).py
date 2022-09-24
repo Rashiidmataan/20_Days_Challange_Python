c83d493142181b299c0de25b58bd692d16226ef7
Abdulrashiid Omar <109062589+Rashiidmataan@users.noreply.github.com>
2022-09-24 10:22:05 +0300

    Add files via upload

---
+++ Operator (Comparison).py
@@ -0,0 +1,51 @@
+#Comparison Operators
+"""
+==: Same
+!=: Not same
+>: Greater than
+<: Less than
+>=: Greater than or Equal
+<=: Less than or equal
+"""
+#Example Same (answers will be True or False)
+
+x = 10
+y = 10
+
+print(x == y)
+
+#Example not same 
+
+x = 10
+y = 10
+
+print(x != y)
+
+#Example greater than 
+
+x = 10
+y = 10
+
+print(x > y)
+
+
+#Example greater than 
+
+x = 10
+y = 10
+
+print(x < y)
+
+#Example greater than or equal
+
+x = 10
+y = 10
+
+print(x >= y)
+
+#Example less than or equal
+
+x = 10
+y = 10
+
+print(x <= y)
